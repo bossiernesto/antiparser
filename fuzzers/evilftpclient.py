7246363ec95254c99254da3333dff3b685b6afc9
bossiernesto <bossi.ernestog@gmail.com>
2015-05-12 23:31:19 -0300

    Reformat code for complying with pep-8 identation

--- fuzzers/evilftpclient.py
+++ fuzzers/evilftpclient.py
@@ -5,7 +5,7 @@
 # This is a complex fuzzer example for testing FTP servers.
 #
 # TODO: Implement more modes
-#	Make fuzzer more intelligent
+# Make fuzzer more intelligent
 #       Test and tweak
 
 import sys
@@ -15,8 +15,9 @@ import string
 import getpass
 from antiparser import *
 
+
 def usage():
-  help = """Usage: python ap-FTPFuzz.py [options] host
+    help = """Usage: python ap-FTPFuzz.py [options] host
         -c, --command [command]	Fuzz a particular FTP command
      	-d, --debug		Set debugging mode.
         -h, --help		Display this help page.
@@ -31,153 +32,155 @@ def usage():
         --save [directory]	Save each permutation to the specified directory.
         NOTE: --save creates a lot of files depending on the fuzzer mode -- one for each payload sent.
   """
-  print help
+    print help
+
 
 def main(argv):
-  # Defaults
-  TERMINATOR = "\r\n"
-  SEPARATOR = " "
-  PORT = 21
-  HOST = "127.0.0.1"
-  DEBUG = False
-  AUTH = False
-  USER = ""
-  PASS = ""
-  CMDFUZZ = False
-  MODE = "default"
-  SLEEP = False
-  TIME = 0
-  SAVE = False
-  PATH = ""
+    # Defaults
+    TERMINATOR = "\r\n"
+    SEPARATOR = " "
+    PORT = 21
+    HOST = "127.0.0.1"
+    DEBUG = False
+    AUTH = False
+    USER = ""
+    PASS = ""
+    CMDFUZZ = False
+    MODE = "default"
+    SLEEP = False
+    TIME = 0
+    SAVE = False
+    PATH = ""
 
-  # List of commands, including several unsupported verbs (does not include a bunch of SITE subverbs)
-  CMDLIST = ['ABOR', 'ALLO', 'APPE', 'CDUP', 'XCUP', 'CWD', 'XCWD', 'DELE', 'HELP', 'LIST', 'MKD',
-             'XMKD', 'MACB', 'MODE', 'MTMD', 'NLST', 'NOOP', 'PASS', 'PASV', 'PORT', 'PWD', 'XPWD',
-             'QUIT', 'REIN', 'RETR', 'RMD', 'XRMD', 'REST', 'RNFR', 'RNTO', 'SITE', 'SIZE', 'STAT',
-             'STOR', 'STRU', 'STOU', 'SYST', 'TYPE', 'USER']
+    # List of commands, including several unsupported verbs (does not include a bunch of SITE subverbs)
+    CMDLIST = ['ABOR', 'ALLO', 'APPE', 'CDUP', 'XCUP', 'CWD', 'XCWD', 'DELE', 'HELP', 'LIST', 'MKD',
+               'XMKD', 'MACB', 'MODE', 'MTMD', 'NLST', 'NOOP', 'PASS', 'PASV', 'PORT', 'PWD', 'XPWD',
+               'QUIT', 'REIN', 'RETR', 'RMD', 'XRMD', 'REST', 'RNFR', 'RNTO', 'SITE', 'SIZE', 'STAT',
+               'STOR', 'STRU', 'STOU', 'SYST', 'TYPE', 'USER']
 
-  # Handle arguments
-  try:
-    opts, args = getopt.getopt(argv, "c:dhH:u:p:P:s:", ["command", "debug", "help", "host=", "user=", 
-                                                        "pass=", "port=", "stdin", "fmt", "glob", "sleep=",
-                                                        "save="])
-  except getopt.GetoptError:
-    usage()
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt in ("-h", "--help"):
-      usage()
-      sys.exit()
-    if opt in ("-c", "--command"):
-      CMD = arg
-    if opt in ("-d", "--debug"):
-      DEBUG = True
-    if opt in ("-u", "--user"):
-      AUTH = True
-      USER = arg
-    if opt in ("-p", "--pass"):
-      AUTH = True
-      PASS = arg
-    if opt in ("-H", "--host"):
-      HOST = arg
-    if opt in ("-P", "--port"):
-      PORT = int(arg)
-    if opt in ("-s", "--sleep"):
-      SLEEP = True
-      TIME = int(arg)
-    if opt in ("--save"):
-      SAVE = True
-      PATH = arg
-    if opt in ("--stdin"):
-      AUTH = True
-      USER = raw_input('Username: ')
-      PASS = getpass.getpass('Password: ')
-    if opt in ("--fmt"):
-      MODE = "fmt"
-    if opt in ("--glob"):
-      MODE = "glob"
+    # Handle arguments
+    try:
+        opts, args = getopt.getopt(argv, "c:dhH:u:p:P:s:", ["command", "debug", "help", "host=", "user=",
+                                                            "pass=", "port=", "stdin", "fmt", "glob", "sleep=",
+                                                            "save="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        if opt in ("-c", "--command"):
+            CMD = arg
+        if opt in ("-d", "--debug"):
+            DEBUG = True
+        if opt in ("-u", "--user"):
+            AUTH = True
+            USER = arg
+        if opt in ("-p", "--pass"):
+            AUTH = True
+            PASS = arg
+        if opt in ("-H", "--host"):
+            HOST = arg
+        if opt in ("-P", "--port"):
+            PORT = int(arg)
+        if opt in ("-s", "--sleep"):
+            SLEEP = True
+            TIME = int(arg)
+        if opt in ("--save"):
+            SAVE = True
+            PATH = arg
+        if opt in ("--stdin"):
+            AUTH = True
+            USER = raw_input('Username: ')
+            PASS = getpass.getpass('Password: ')
+        if opt in ("--fmt"):
+            MODE = "fmt"
+        if opt in ("--glob"):
+            MODE = "glob"
 
-  table = string.maketrans('', '')
-  illegal = string.translate(table, table, string.ascii_letters + string.digits)
-    
-  # set up antiparser
+    table = string.maketrans('', '')
+    illegal = string.translate(table, table, string.ascii_letters + string.digits)
 
-  for cmd in CMDLIST:
-    ap = antiparser()      
-    cmdkw = apKeywords()
-    if DEBUG:
-      cmdkw.setDebug(True)
-    cmdkw.setKeywords([cmd])
-    cmdkw.setSeparator(SEPARATOR)
-    cmdkw.setTerminator(TERMINATOR)
+    # set up antiparser
 
-    if MODE == "fmt":
-      cmdkw.setContent(r"%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n")
-      cmdkw.setOptional(True)
-    elif MODE == "glob":
-      illegal = string.translate(table, table, "/.{}~*?")
-      cmdkw.setIllegalChars(illegal)
-    else:
-      cmdkw.setIllegalChars(illegal)
-    cmdkw.setMode('incremental')
-    cmdkw.setMaxSize(65536)
-    ap.append(cmdkw)
+    for cmd in CMDLIST:
+        ap = antiparser()
+        cmdkw = apKeywords()
+        if DEBUG:
+            cmdkw.setDebug(True)
+        cmdkw.setKeywords([cmd])
+        cmdkw.setSeparator(SEPARATOR)
+        cmdkw.setTerminator(TERMINATOR)
 
-    if MODE == "default" or MODE == "glob": 
-      if MODE == "glob":
-        CMDLIST = ['APPE', 'CDUP', 'XCUP', 'CWD', 'XCWD', 'DELE', 'LIST', 'MKD',
-                   'XMKD', 'MDTM', 'NLST', 'PWD', 'XPWD', 'RETR', 'RMD', 'XRMD', 
-                   'RNFR', 'RNTO', 'STOR', 'STOU']
+        if MODE == "fmt":
+            cmdkw.setContent(r"%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n")
+            cmdkw.setOptional(True)
+        elif MODE == "glob":
+            illegal = string.translate(table, table, "/.{}~*?")
+            cmdkw.setIllegalChars(illegal)
+        else:
+            cmdkw.setIllegalChars(illegal)
+        cmdkw.setMode('incremental')
+        cmdkw.setMaxSize(65536)
+        ap.append(cmdkw)
 
-      for i in xrange(1, 65):
-        ap.permute()
-        sock = apSocket()
-        print "++ Connecting to Server: %s %s" % (HOST, PORT)
-        sock.connect(HOST, PORT)
-        # print banner
-        print sock.recv(10240)
-        if AUTH:
-          print "++ Sending USER credentials ++"
-          sock.sendTCP("USER " + USER + TERMINATOR)
-          print sock.recv(10240)
-          print "++ Sending PASS credentials ++"
-          sock.sendTCP("PASS " + PASS + TERMINATOR)
-          print sock.recv(10240)
-        
-        print "++ Sending command: %s Length: %s ++" % (cmd, cmdkw.getContentSize())
-        sock.sendTCP(ap.getPayload())
-        print sock.recv(1024)
-        sock.close()
-        if SAVE:
-          file = PATH + "/" + cmd + "fuzz" + str(i)
-          print "++ Saving permutation as %s ++" % file
-          ap.save(file)
-        if SLEEP:
-          sock.sleep(TIME)
+        if MODE == "default" or MODE == "glob":
+            if MODE == "glob":
+                CMDLIST = ['APPE', 'CDUP', 'XCUP', 'CWD', 'XCWD', 'DELE', 'LIST', 'MKD',
+                           'XMKD', 'MDTM', 'NLST', 'PWD', 'XPWD', 'RETR', 'RMD', 'XRMD',
+                           'RNFR', 'RNTO', 'STOR', 'STOU']
 
-    if MODE == "fmt":
-      sock = apSocket()
-      print "++ Connecting to server: %s %s ++" % (HOST, PORT)
-      sock.connect(HOST, PORT)
-      # print banner
-      print sock.recv(1024)
-      if AUTH:
-        print "++ Sending USER credentials ++"
-        sock.sendTCP("USER " + USER + TERMINATOR)
-        print sock.recv(1024)
-        print "++ Sending PASS credentials ++"
-        sock.sendTCP("PASS " + PASS + TERMINATOR)
-        print sock.recv(1024)
-      print "++ Sending command: %s (Format String Mode) ++" % cmd
-      sock.sendTCP(ap.getPayload())
-      print sock.recv(1024)
-      sock.close()
-      if SAVE:
-        file = PATH + "/" + cmd + "fuzz" + str(i)
-        print "++ Saving permutation as %s ++" % file
-        ap.save(file)
-      if SLEEP:
-        sock.sleep(TIME)
+            for i in xrange(1, 65):
+                ap.permute()
+                sock = apSocket()
+                print "++ Connecting to Server: %s %s" % (HOST, PORT)
+                sock.connect(HOST, PORT)
+                # print banner
+                print sock.recv(10240)
+                if AUTH:
+                    print "++ Sending USER credentials ++"
+                    sock.sendTCP("USER " + USER + TERMINATOR)
+                    print sock.recv(10240)
+                    print "++ Sending PASS credentials ++"
+                    sock.sendTCP("PASS " + PASS + TERMINATOR)
+                    print sock.recv(10240)
+
+                print "++ Sending command: %s Length: %s ++" % (cmd, cmdkw.getContentSize())
+                sock.sendTCP(ap.getPayload())
+                print sock.recv(1024)
+                sock.close()
+                if SAVE:
+                    file = PATH + "/" + cmd + "fuzz" + str(i)
+                    print "++ Saving permutation as %s ++" % file
+                    ap.save(file)
+                if SLEEP:
+                    sock.sleep(TIME)
+
+        if MODE == "fmt":
+            sock = apSocket()
+            print "++ Connecting to server: %s %s ++" % (HOST, PORT)
+            sock.connect(HOST, PORT)
+            # print banner
+            print sock.recv(1024)
+            if AUTH:
+                print "++ Sending USER credentials ++"
+                sock.sendTCP("USER " + USER + TERMINATOR)
+                print sock.recv(1024)
+                print "++ Sending PASS credentials ++"
+                sock.sendTCP("PASS " + PASS + TERMINATOR)
+                print sock.recv(1024)
+            print "++ Sending command: %s (Format String Mode) ++" % cmd
+            sock.sendTCP(ap.getPayload())
+            print sock.recv(1024)
+            sock.close()
+            if SAVE:
+                file = PATH + "/" + cmd + "fuzz" + str(i)
+                print "++ Saving permutation as %s ++" % file
+                ap.save(file)
+            if SLEEP:
+                sock.sleep(TIME)
+
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
